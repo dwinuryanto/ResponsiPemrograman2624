5f820a94d15ea083f4bf7312153110c148f1b625
dwinuryanto <yantodwinur29@gmail.com>
2020-04-13 14:36:41 +0700

    Responsi

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krw = new karyawan
+            {
+                nama1 = "dwi",
+                nik1 = "2624",
+                gaji1 = 3000000,
+
+                nama2 = "saputro",
+                nik2 = "2555",
+                gaji2 = 2000000
+            };
+
+            krw.infoGaji();
+            Console.WriteLine();
+            krw.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace responsi
+{
+    class karyawan
+    {
+        public string nama1 { get; set; }
+        public string nik1 { get; set; }
+        public int gaji1 { get; set; }
+        public string nama2 { get; set; }
+        public string nik2 { get; set; }
+        public int gaji2 { get; set; }
+        public void infoGaji()
+        {
+            Console.WriteLine("No\tNim/Nama\t\tGaji");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("1\t{0}\t{1}\t\t{2}", nik1, nama1, gaji1);
+            Console.WriteLine("2\t{0}\t{1}\t\t{2}", nik2, nama2, gaji2);
+        }
+        public void kenaikanGaji()
+        {
+            if (gaji1 < 0)
+            {
+                gaji1 = 0;
+            }
+            else if (gaji2 < 0)
+            {
+                gaji2 = 0;
+            }
+            Console.WriteLine("\nasiikkk gaji naik 10%");
+            Console.WriteLine("\nNo\tNik/Nama\t\tGaji");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("1\t{0}\t{1}\t\t{2}", nik1, nama1, gaji1 + (gaji1 * 0.1));
+            Console.WriteLine("2\t{0}\t{1}\t\t{2}", nik2, nama2, gaji2 + (gaji2 * 0.1));
+        }
+    }
+}
